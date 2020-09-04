85014f0c63b8c4308573e15681ecfed9f8ae65ee
Radu Nichita <45298861+RaduNichita@users.noreply.github.com>
2020-09-04 20:38:59 +0300

    added dialog alert on exit button and back button (#1)
    * added dialog alert on exit button and back button
    
    Co-authored-by: m4r1nn <marian.burcea@stud.acs.upb.ro>

--- app/src/main/java/com/example/saboteur/MainActivity.java
+++ app/src/main/java/com/example/saboteur/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.saboteur;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exitApp(View view) {
+        Log.d(LOG_TAG, "exitApp");
+        showExitDialog();
+    }
+
+    public void showExitDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog = builder.setMessage("Are you sure?")
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                })
+                .create();
+        dialog.show();
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(R.drawable.bgd_dialog);
+    }
+
+    @Override
+    public void onBackPressed() {
+        showExitDialog();
     }
 }
